d0c263423fda5e8d3627ee765a68e5a869d27bc9
Mikel Cebreiro <mikelcebreiro@MacBook-Air-de-Mikel.local>
2023-11-16 18:35:43 -0300

    register y login version 1.0

--- src/components/user/RegisterPage.jsx
+++ src/components/user/RegisterPage.jsx
@@ -23,34 +23,50 @@ const RegisterPage = (props) =>{
     }
 
 return (    
-    <form onSubmit={handleSubmit}>
-        <h2>Register</h2> <br />
-        <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={register.email}
-            onChange={handleChange}    
-        ></input>
-        <br />
-        <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={register.password}
-            onChange={handleChange}    
-        ></input>
-        <br />
-        <input
-            type="name"
-            name="name"
-            placeholder="Nombre"
-            value={register.name}
-            onChange={handleChange}    
-        ></input>
-        <br />
-        <button type="submit">Register</button>
-       </form>
+    <section class ="bg-[#fefae0] min-h-screen flex items-center justify-center">
+    <div class= "bg-[#79B473] flex rounded-2xl shadow-lg max-w-3xl p-5"> 
+     {/* login container */}
+     <div class="md: w-1/2 px-12">
+    <form onSubmit={handleSubmit} class="flex flex-col gap-4">
+<h2 class="font-mono text-2xl flex items-center justify-center">Bienvenido</h2>
+<h2 class="font-mono text-2xl flex items-center justify-center">al work-café</h2>
+<p class="text-sm mt-4 font-mono flex items-center justify-center"> Ingrese sus datos</p>
+<input class="p-2 mt-8 font-mono rounded-xl border"
+    type="name"
+    name="name"
+    placeholder="Nombre"
+    value={register.name}
+    onChange={handleChange} ></input>
+<input class="p-2 mt-5 font-mono rounded-xl border"
+    type="email"
+    name="email"
+    placeholder="Email"
+    value={register.email}
+    onChange={handleChange} >
+
+    </input>
+<input class="p-2 mt-5 font-mono rounded-xl border"
+    type="password"
+    name="password"
+    placeholder="Password"
+    value={register.password}
+    onChange={handleChange}></input>
+<button class="bg-[#6F8F72] font-mono rounded-xl py-2 hover:scale-105 duration-300" type="submit">Registrarme</button>
+</form>
+    </div>
+{/* image container  */}
+
+<div class="md:block hidden w-1/2">
+<img class="rounded-2xl" src="/images/test2.jpeg" alt="" />
+       
+    </div>
+   
+    </div>
+
+
+</section>
     );
+
+    
 }
 export default RegisterPage;
