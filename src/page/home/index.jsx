2a2f3cac038e2e580f786ae15664b62337163005
JuanSosa17 <juan.sosav17@gmail.com>
2023-11-26 16:01:40 -0300

    Lista de reservas

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,19 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import NavBarComponent from "../../components/navbar";
-import bookings from "./bookings";
 import ReservasList from "../../components/reservas/ReservasList"
 
 export default function Home() {
   const navigate = useNavigate();
+  const [bookings, setBookings] = useState({});
+
+
+  const callBookings = async () => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+          'Content-Type': 'application/json',
+          'token': localStorage.getItem("token")
+      },
+    }
+
+      await fetch("http://localhost:4000/api/bookings/find-for-user", requestOptions)
+      .then(response => {
+          console.log(response);
+          return response.text();
+      })
+      .then(data => {
+          console.log("data", data);
+          const newData = JSON.parse(data);
+          setBookings(newData);
+          return newData;
+      })
+  }
 
   useEffect(() => {
     console.log(localStorage.getItem("token"));
     if (!localStorage.getItem("token")) {
       navigate('/login')
+    }else{
+      callBookings();
+      console.log(bookings)
     }
   }, []);
 
+  
   return (
     <>
       <NavBarComponent />
@@ -21,6 +48,7 @@ export default function Home() {
       <h3>Esta es tu lista de reservas:</h3>
       <ReservasList bookings={bookings} />
       <Link to="/create-booking">Crear Reserva</Link>
+      
     </>
   )
 }
