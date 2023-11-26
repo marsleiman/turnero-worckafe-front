798883d147c6598e255f45eb9b482c64a79a9b02
Mariam SLeiman <msleiman@santandertecnologia.com.ar>
2023-11-26 16:27:58 -0300

    home spinnr

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -6,27 +6,28 @@ import ReservasList from "../../components/reservas/ReservasList"
 export default function Home() {
   const navigate = useNavigate();
   const [bookings, setBookings] = useState({});
+  const [spiner, setSpiner] = useState(true);
 
 
   const callBookings = async () => {
     const requestOptions = {
       method: "GET",
       headers: {
-          'Content-Type': 'application/json',
-          'token': localStorage.getItem("token")
+        'Content-Type': 'application/json',
+        'token': localStorage.getItem("token")
       },
     }
 
-      await fetch("http://localhost:4000/api/bookings/find-for-user", requestOptions)
+    await fetch("http://localhost:4000/api/bookings/find-for-user", requestOptions)
       .then(response => {
-          console.log(response);
-          return response.text();
+        console.log(response);
+        return response.text();
       })
       .then(data => {
-          console.log("data", data);
-          const newData = JSON.parse(data);
-          setBookings(newData);
-          return newData;
+        console.log("data", data);
+        const newData = JSON.parse(data);
+        setBookings(newData);
+        return newData;
       })
   }
 
@@ -34,21 +35,23 @@ export default function Home() {
     console.log(localStorage.getItem("token"));
     if (!localStorage.getItem("token")) {
       navigate('/login')
-    }else{
+    } else {
       callBookings();
-      console.log(bookings)
+      console.log('HOLAAAAAA', bookings)
+      setTimeout(() => setSpiner(false), 10000);
     }
   }, []);
 
-  
+
   return (
     <>
-      <NavBarComponent />
-      <h1>Bienvenido!</h1>
-      <h3>Esta es tu lista de reservas:</h3>
-      <ReservasList bookings={bookings} />
-      <Link to="/create-booking">Crear Reserva</Link>
-      
+      {spiner ? <span>cargando</span> :
+        (<>
+          <NavBarComponent />
+          <h1>Bienvenido!</h1>
+          <h3>Esta es tu lista de reservas:</h3>
+          <ReservasList bookings={bookings} />
+          <Link to="/create-booking">Crear Reserva</Link></>)}
     </>
   )
 }
