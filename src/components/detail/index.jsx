56c17a3ed78d0a584f204c699458f1c236d86607
Mikel Cebreiro <mikelcebreiro@MacBook-Air-de-Mikel.local>
2023-11-26 19:23:42 -0300

    último :)

--- src/components/detail/index.jsx
+++ src/components/detail/index.jsx
@@ -1,49 +1,57 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import deleteBooking from "../../servicies/deleteBooking";
 
 export default function ComponentDetail({ detail }) {
-    const [detailProces, setDetailProces] = useState(detail);
-    const [resp, setResp] = useState();
+  const [detailProces, setDetailProces] = useState(detail);
+  const [resp, setResp] = useState();
 
-    const call = async () => {
-        const respons = await deleteBooking(detail._id);
-        console.log('respo', respons);
-        setResp(respons)
-    } 
-    
-    const handleDelte = async (event) => {
-        event.preventDefault();
+  const call = async () => {
+    const respons = await deleteBooking(detail._id);
+    console.log("respo", respons);
+    setResp(respons);
+  };
 
-        try {
-            call();
-        } catch {
-            setResp(null)
-        }
+  const handleDelte = async (event) => {
+    event.preventDefault();
 
-        setDetailProces(null)
+    try {
+      call();
+    } catch {
+      setResp(null);
     }
 
-    function DetailComp() {
-        return <>
-            <span>{detail.dia}</span>
-            <span>{detail.room_id}</span>
-            <span>{detail.capacity}</span>
-            <button onClick={(e) => handleDelte(e)}>Cancelar Reserva</button>
-        </>
-    }
+    setDetailProces(null);
+  };
 
-    function CheckExist() {
-        if (!detailProces && !resp) {
-          return <span>No existe la reserva :s</span>
-        } else if (!detailProces && resp) {
-            return <span>Reserva cancelada!</span>
-        }
-      }
-
+  function DetailComp() {
     return (
-        <>
-            {detailProces && <DetailComp />}
-            {<CheckExist />}
-        </>
-    )
+      <>
+        <div className="font-mono text-md flex flex-col items-center justify-center gap-4 mt-4 py-2 ">
+          <ul class>Fecha: {detail.dia}</ul>
+          <ul>Sala: {detail.room_id}</ul>
+          <ul>Capacidad: {detail.capacity}</ul>
+        </div>
+
+        <button
+          onClick={(e) => handleDelte(e)}
+          className="bg-[#6F8F72] font-mono rounded-xl p-10 flex items-center justify-center py-2 mt-3 hover:scale-105 duration-300"
+        >
+          Cancelar Reserva
+        </button>
+      </>
+    );
+  }
+
+  function CheckExist() {
+    if (!detailProces && resp) {
+      return <span>Reserva cancelada!</span>;
+    }
+  }
+
+  return (
+    <>
+      {detailProces && <DetailComp />}
+      {<CheckExist />}
+    </>
+  );
 }
--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -47,7 +47,6 @@ export default function Home() {
   return (
     <>
       <NavBarComponent />
-
       <Card
         title="Bienvenido!"
         subtitle="Esta es tu lista de reservas"
